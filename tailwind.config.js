cbc24ff482df376ce04e5df80df707c501392aee
Eduardo Narez <em5577595@gmail.com>
2024-02-03 21:56:08 -0600

    ADD: Configuración tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@ module.exports = {
   prefix: '',
   important: false,
   separator: ':',
+  content: [
+    "./src/**/*.html",
+    "./src/**/*.js",
+    "./src/input.css", // Ruta a tu archivo de entrada
+  ],
   theme: {
     screens: {
       sm: '640px',
